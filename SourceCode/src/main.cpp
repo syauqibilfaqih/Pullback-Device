cadd4a86136ad25cbd8f1b1edce7321822d0bfb2
syauqibilfaqih <alisyauqibilfaqih@gmail.com>
2024-08-21 20:30:14 +0700

    Implemented OTA to the firmware

--- SourceCode/src/main.cpp
+++ SourceCode/src/main.cpp
@@ -1,5 +1,9 @@
 
 #include <Arduino.h>
+#include <ArduinoOTA.h>
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
 #include <U8x8lib.h>
 #include <U8g2lib.h>
 #include <analogWrite.h>
@@ -7,6 +11,10 @@
 #ifdef U8X8_HAVE_HW_SPI
 #include <SPI.h>
 #endif
+
+// WiFi SSID & Password
+const char* ssid = "VICTUS 0978";
+const char* password = "5Vr7434/";
 
 U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
@@ -264,6 +272,44 @@ void setup(void)
   u8g2.sendBuffer();
   //u8x8.refreshDisplay();		// only required for SSD1606/7  
   delay(2000);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
   u8g2.clearBuffer();
   u8g2.clearDisplay();
   u8x8.drawString(0,3," Vascular");
@@ -277,5 +323,6 @@ void setup(void)
 
 void loop(void)
 {
+  ArduinoOTA.handle();
   mainProgram();
 }
